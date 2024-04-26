376655212f20696469d2fc2254e283a558ccb2f5
Natan Maia <natanmaiaplays@outlook.com>
2024-04-26 02:37:14 -0300

    busca?

--- classes/directory.hpp
+++ classes/directory.hpp
@@ -164,6 +164,22 @@ public:
         }
     }
 
+    int buscarBuckets(int ano) {
+        int anoHashed = hashFunction(ano, pg);
+        int ref = diretorio[anoHashed].third;
+        int profLocal = diretorio[anoHashed].second;
+        string refBinary = to_binary(ref, profLocal);
+        string filename = refBinary + ".txt";
+
+        bucketAtual = Bucket::read(filename);
+        int count = 0;
+        for (pair<int, int> p : bucketAtual->values) {
+            if (p.second == ano) count++;
+        }
+        delete bucketAtual;
+        return count;
+    }
+
     void duplicarBucket(int dirIndex, int ano) {
         // Indice a ser duplicado
         tripla<int,int,int> ind = this->diretorio[dirIndex];
